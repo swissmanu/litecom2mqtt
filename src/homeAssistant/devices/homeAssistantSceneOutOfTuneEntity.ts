dc828730355f747f4974e907e083222826c83366
Manuel Alabor <manuel@alabor.me>
2023-11-01 23:36:37 +0100

    Add Scene Out Of Tune Entity

---
+++ src/homeAssistant/devices/homeAssistantSceneOutOfTuneEntity.ts
@@ -0,0 +1,38 @@
+import { Device, Zone } from '../../litecom/restClient/index.js';
+import { Config } from '../../util/config.js';
+import { HomeAssistantEntity, HomeAssistantEntityType, DataPointType } from './homeAssistantEntity.js';
+
+/**
+ * @see https://www.home-assistant.io/integrations/sensor.mqtt/
+ */
+export class HomeAssistantSceneOutOfTuneEntity extends HomeAssistantEntity {
+    override homeAssistantEntityType: HomeAssistantEntityType = 'sensor';
+    override dataPointType: DataPointType = 'sceneOutOfTune';
+
+    constructor(config: Config, zone: Zone, device?: Device) {
+        super(config, zone, device);
+    }
+
+    override get homeAssistantCommandTopics(): Record<string, string> {
+        return {};
+    }
+
+    override get homeAssistantEntityConfig(): Record<string, string | number | boolean | string[]> {
+        const shared = {
+            icon: 'mdi:tune-variant',
+            name: 'Scene Out Of Tune',
+        };
+
+        if (this.device) {
+            return {
+                ...shared,
+                state_topic: `${this.config.LITECOM2MQTT_LITECOM_STATE_MQTT_TOPIC_PREFIX}/zones/${this.zone.id}/devices/${this.device.id}/services/scene/outOfTune`,
+            };
+        }
+
+        return {
+            ...shared,
+            state_topic: `${this.config.LITECOM2MQTT_LITECOM_STATE_MQTT_TOPIC_PREFIX}/zones/${this.zone.id}/services/scene/outOfTune`,
+        };
+    }
+}
